1dfc664ad70ea0e5a91ed697dc0421c281295894
Austin Casanova <austin_casanova@hotmail.com>
2026-01-08 17:18:16 +0000

    igress egress

--- data.tf
+++ data.tf
@@ -12,3 +12,37 @@ resource "aws_security_group" "allow_tls" {
     Name = "Kiran_sg_group_Boa"
   }
 }
+
+#Ingress rule port 22
+resource "aws_vpc_security_group_ingress_rule" "example22" {
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+#Ingress rule
+resource "aws_vpc_security_group_ingress_rule" "example80" {
+  security_group_id = aws_security_group.allow_tls.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+#Egress
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+ 
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "ec2-t3" {
   ami           = var.ec2-ami
   instance_type = var.vm-size
-
+#attaching security group
+vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
+ 
   tags = {
     Name = var.vm-name
   }
